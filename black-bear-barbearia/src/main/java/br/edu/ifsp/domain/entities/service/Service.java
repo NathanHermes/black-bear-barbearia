734cf4833b593dd3f9f049aa8aa9801cd28ad6e8
dudasantos2522 <mariaeduardasantos0205@gmail.com>
2022-10-26 22:01:39 -0300

    Service iniciado

--- black-bear-barbearia/src/main/java/br/edu/ifsp/domain/entities/service/Service.java
+++ black-bear-barbearia/src/main/java/br/edu/ifsp/domain/entities/service/Service.java
@@ -1,4 +1,33 @@
 package br.edu.ifsp.domain.entities.service;
 
+import br.edu.ifsp.domain.entities.booking.Booking;
+
+import java.math.BigDecimal;
+
 public class Service {
+    private Integer id;
+    private String nome;
+    private BigDecimal price;
+    private BigDecimal comissionPercentage;
+    private BigDecimal taxPercentage;
+    private Boolean active;
+
+    private Type type;
+    private Booking booking;
+
+    public void addType(Type type) {}
+    public void removeType(Type type) {}
+    public boolean isOneOfType(Type type) {
+        return false;
+    }
+    public BigDecimal calculateComission() {
+        return null;
+    }
+    public BigDecimal calculateTax() {
+        return null;
+    }
+    public BigDecimal calculateNetPrice() {
+        return null;
+    }
+
 }
