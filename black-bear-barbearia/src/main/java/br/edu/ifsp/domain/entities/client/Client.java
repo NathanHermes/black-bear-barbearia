472559e90853e0d5a9a07dd6d217951984919d38
dudasantos2522 <mariaeduardasantos0205@gmail.com>
2022-10-26 22:01:54 -0300

    Client iniciado

--- black-bear-barbearia/src/main/java/br/edu/ifsp/domain/entities/client/Client.java
+++ black-bear-barbearia/src/main/java/br/edu/ifsp/domain/entities/client/Client.java
@@ -1,4 +1,14 @@
 package br.edu.ifsp.domain.entities.client;
 
+import br.edu.ifsp.domain.entities.booking.Booking;
+
+import java.util.ArrayList;
+
 public class Client {
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+
+    private ArrayList<Booking> bookings;
 }
