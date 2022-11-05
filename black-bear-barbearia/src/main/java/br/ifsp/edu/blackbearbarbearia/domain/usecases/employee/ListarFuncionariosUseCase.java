bb40d4006a54237ce4ba960f1d3905c2d6a6336a
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-05 15:22:12 -0300

    Implementação da classe ListarFuncionariosUseCase

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/ListarFuncionariosUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/ListarFuncionariosUseCase.java
@@ -1,4 +1,23 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.employee;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.user.UserDAO;
+
+import java.util.List;
+
 public class ListarFuncionariosUseCase {
+    private final UserDAO dao;
+
+    public ListarFuncionariosUseCase(UserDAO dao) {
+        this.dao = dao;
+    }
+
+    public List<User> findAll() {
+        List<User> users = dao.findAll();
+
+        if (users.isEmpty())
+            throw new IllegalArgumentException("No user found.");
+
+        return users;
+    }
 }
