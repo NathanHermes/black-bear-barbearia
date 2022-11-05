18a7fa66ee1ce4424c47c943d177515884363607
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-05 19:30:45 -0300

    Implementação da classe ServiceInputRequestValidator

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/ServiceInputRequestValidator.java
@@ -0,0 +1,27 @@
+package br.ifsp.edu.blackbearbarbearia.domain.usecases.service;
+
+import br.ifsp.edu.blackbearbarbearia.domain.entities.service.Service;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
+import java.util.Collection;
+
+public class ServiceInputRequestValidator extends Validator<Service> {
+    @Override
+    public Notification validate(Service service) {
+        Notification notification = new Notification();
+
+        if (service == null) {
+            notification.addError("Service is null");
+            return notification;
+        }
+
+        if (nullOrEmpty(service.getNome())) notification.addError("Name is null or empty.");
+        if (nullOrEmpty(String.valueOf(service.getPrice()))) notification.addError("Price is null or empty.");
+        if (nullOrEmpty(String.valueOf(service.getComissionPercentage()))) notification.addError("Commission is null or empty.");
+        if (nullOrEmpty(String.valueOf(service.getTaxPercentage()))) notification.addError("Taxa percentage is null or empty.");
+
+
+        return notification;
+    }
+}
