6d95995a5bd7d1fbf0fa39f9f81b007bd339303b
NathanHermes <hermesnathan8@gmail.com>
2022-11-05 15:26:12 -0300

    Implementação inicial da função 'update' da classe EditarFuncionarioUseCase.

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/EditarFuncionarioUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/EditarFuncionarioUseCase.java
@@ -1,4 +1,29 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.employee;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.user.UserDAO;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.EntityNotFoundException;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
 public class EditarFuncionarioUseCase {
+    private final UserDAO dao;
+
+    public EditarFuncionarioUseCase(UserDAO dao) {
+        this.dao = dao;
+    }
+
+    public boolean update(User employee) {
+        Validator<User> validator = new EmployeeInputRequestValidator();
+        Notification notification = validator.validate(employee);
+
+        if (notification.hasErros())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        String login = employee.getLogin();
+        if (dao.findOneByLogin(login).isEmpty())
+            throw new EntityNotFoundException(login);
+
+        return dao.update(employee);
+    }
 }
