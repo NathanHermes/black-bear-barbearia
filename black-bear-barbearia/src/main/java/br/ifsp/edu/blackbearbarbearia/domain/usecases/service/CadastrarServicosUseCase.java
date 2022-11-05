9313058200f5a9e9ef7cf73e71aecd6122bf40e2
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-05 19:26:39 -0300

    Implementação da classe CadastrarServicosUseCase

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/CadastrarServicosUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/CadastrarServicosUseCase.java
@@ -1,4 +1,28 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.service;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.service.Service;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.EntityAlreadyExistsException;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
 public class CadastrarServicosUseCase {
+    private final ServiceDAO dao;
+
+    public CadastrarServicosUseCase(ServiceDAO dao) {
+        this.dao = dao;
+    }
+
+    public Integer create(Service service) {
+        Validator<Service> validator = new ServiceInputRequestValidator();
+        Notification notification = validator.validate(service);
+
+        if (notification.hasErros())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        String name = service.getNome();
+        if (dao.findOneByName(name).isPresent())
+            throw new EntityAlreadyExistsException("This name is already in use.");
+
+        return dao.create(service);
+    }
 }
