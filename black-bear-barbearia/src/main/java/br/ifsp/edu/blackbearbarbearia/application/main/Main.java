d457330ab24565746c1ad0e065815af1738266cc
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-05 16:29:50 -0300

    Implementação inicial do menu

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/main/Main.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/main/Main.java
@@ -14,27 +14,36 @@ public class Main {
     public static LoginUseCase loginUseCase;
     public static CadastrarFuncionarioUseCase cadastrarFuncionarioUseCase;
     private static User user = null;
+    private Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         configureInjection();
         createAdmin();
 
-        final Scanner input = new Scanner(System.in);
+        Main main = new Main();
+        int opcao = -1;
 
-        while (user == null) {
-            System.out.println("> Login:");
-            String login = input.nextLine();
-            System.out.println("> Password:");
-            String password = input.nextLine();
+        main.login();
 
-            try {
-                user = new User(login, password);
-                if (loginUseCase.login(user) != null) System.out.println("[   LOGIN EFETUADO   ]");
-            } catch (Exception e) {
-                user = null;
-                System.out.println(e.getMessage() + "\n[... TENTE NOVAMENTE ...]\n");
+        while (opcao != 0) {
+            opcao = main.menu();
+            switch (opcao) {
+                case 1 -> createEmployee();
+                case 2 -> updateEmployee();
+                case 3 -> listEmployee();
             }
         }
     }
+
+    public int menu() {
+        System.out.println("0. Sair");
+        System.out.println("1. Create employee");
+        System.out.println("2. Update employee");
+        System.out.println("3. List employees");
+        System.out.println("Escolha: ");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
     private static void configureInjection() {
         UserDAO userDAO = new InMemoryUserDAO();
         cadastrarFuncionarioUseCase = new CadastrarFuncionarioUseCase(userDAO);
@@ -53,6 +62,32 @@ public class Main {
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
+    }
 
+    private void login() {
+        while (user == null) {
+            System.out.println("> Login:");
+            String login = scanner.nextLine();
+            System.out.println("> Password:");
+            String password = scanner.nextLine();
+
+            try {
+                user = new User(login, password);
+                if (loginUseCase.login(user) != null) System.out.println("[   LOGIN EFETUADO   ]");
+            } catch (Exception e) {
+                user = null;
+                System.out.println(e.getMessage() + "\n[... TENTE NOVAMENTE ...]\n");
+            }
+        }
+    }
+
+    private static void createEmployee() {
+    }
+
+    private static void updateEmployee() {
+    }
+
+    private static void listEmployee() {
     }
 }
+
