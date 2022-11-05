2190c3bf51a53fbe7a452394a8019da1fec9614c
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-05 15:50:42 -0300

    CadastrarFuncionarioUseCase vazio

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/CadastrarFuncionarioUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/CadastrarFuncionarioUseCase.java
@@ -1,30 +1,4 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.employee;
 
-import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
-import br.ifsp.edu.blackbearbarbearia.domain.usecases.user.UserInputRequestValidator;
-import br.ifsp.edu.blackbearbarbearia.domain.usecases.user.UserDAO;
-import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.EntityAlreadyExistsException;
-import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
-import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
-
 public class CadastrarFuncionarioUseCase {
-    private final UserDAO dao;
-
-    public CadastrarFuncionarioUseCase(UserDAO dao) {
-        this.dao = dao;
-    }
-
-    public Integer create(User employee) {
-        Validator<User> validator = new EmployeeInputRequestValidator();
-        Notification notification = validator.validate(employee);
-
-        if (notification.hasErros())
-            throw new IllegalArgumentException(notification.errorMessage());
-
-        String login = employee.getLogin();
-        if (dao.findOneByLogin(login).isPresent())
-            throw new EntityAlreadyExistsException("This login is already in use.");
-
-        return dao.create(employee);
-    }
 }
