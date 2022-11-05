810fd8dbec520231809821a8c9489b248ba7c4bf
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-05 13:53:56 -0300

    Implementação da classe ListarClientesUseCase

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/client/ClientDAO.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/client/ClientDAO.java
@@ -1,11 +1,10 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.client;
 
 import br.ifsp.edu.blackbearbarbearia.domain.entities.client.Client;
-import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.DAO;
 
-public interface ClientDAO extends DAO<Client, Integer> {
-    User findOneByEmail(String email);
+import java.util.Optional;
 
-    User findOne(String id);
+public interface ClientDAO extends DAO<Client, Integer> {
+    Optional<Client> findOneByEmail(String email);
 }
--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/client/ListarClientesUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/client/ListarClientesUseCase.java
@@ -1,4 +1,23 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.client;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.client.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListarClientesUseCase {
+    private final ClientDAO dao;
+
+    public ListarClientesUseCase(ClientDAO dao) {
+        this.dao = dao;
+    }
+
+    public List<Client> list() {
+        List<Client> clients = dao.findAll();
+
+        if (clients.isEmpty())
+            throw new IllegalArgumentException("No client found.");
+
+        return clients;
+    }
 }
