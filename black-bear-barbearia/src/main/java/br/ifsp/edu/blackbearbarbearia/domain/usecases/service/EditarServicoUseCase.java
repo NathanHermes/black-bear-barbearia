a80f842e819702c979ea64f092e8fc80c2910479
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-05 19:27:09 -0300

    Implementação da classe EditarServicoUseCase

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/EditarServicoUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/EditarServicoUseCase.java
@@ -1,4 +1,28 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.service;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.service.Service;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.EntityNotFoundException;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
 public class EditarServicoUseCase {
+    private final ServiceDAO dao;
+
+    public EditarServicoUseCase(ServiceDAO dao) {
+        this.dao = dao;
+    }
+
+    public boolean update(Service service) {
+        Validator<Service> validator = new ServiceInputRequestValidator();
+        Notification notification = validator.validate(service);
+
+        if (notification.hasErros())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        String name = service.getNome();
+        if(dao.findOneByName(name).isEmpty())
+            throw new EntityNotFoundException(name);
+
+        return dao.update(service);
+    }
 }
