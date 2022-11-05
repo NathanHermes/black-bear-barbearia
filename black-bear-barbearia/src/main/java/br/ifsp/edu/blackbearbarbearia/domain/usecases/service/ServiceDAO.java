f0ecaa82d9322078cc2ac2bde3590b93db5c40d5
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-05 19:30:17 -0300

    Implementação da interface ServiceDAO

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/ServiceDAO.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/ServiceDAO.java
@@ -1,7 +1,11 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.service;
 
 import br.ifsp.edu.blackbearbarbearia.domain.entities.service.Service;
+import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.DAO;
 
+import java.util.Optional;
+
 public interface ServiceDAO extends DAO<Service, Integer> {
+    Optional<Service> findOneByName(String name);
 }
