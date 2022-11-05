0545690c0adbaf4ffeef75bf2a64fb2b70a0fd01
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-05 19:27:47 -0300

    Implementação da classe ListarServicosUseCase

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/ListarServicosUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/ListarServicosUseCase.java
@@ -1,4 +1,22 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.service;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.service.Service;
+
+import java.util.List;
+
 public class ListarServicosUseCase {
+    private final ServiceDAO dao;
+
+    public ListarServicosUseCase(ServiceDAO dao) {
+        this.dao = dao;
+    }
+
+    public List<Service> findAll() {
+        List<Service> services = dao.findAll();
+
+        if (services.isEmpty())
+            throw new IllegalArgumentException("No service found.");
+
+        return services;
+    }
 }
