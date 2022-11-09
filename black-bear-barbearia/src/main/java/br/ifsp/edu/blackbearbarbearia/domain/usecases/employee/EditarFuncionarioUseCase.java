6494448eb170f16a1e89baf43046df128be3e6fd
NathanHermes <hermesnathan8@gmail.com>
2022-11-08 21:53:01 -0300

    feat: implementação do método update

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/EditarFuncionarioUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/EditarFuncionarioUseCase.java
@@ -1,29 +1,37 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.employee;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.user.Address;
 import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.user.UserDAO;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.EntityNotFoundException;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
 
+import java.util.Optional;
+
 public class EditarFuncionarioUseCase {
     private final UserDAO dao;
-
     public EditarFuncionarioUseCase(UserDAO dao) {
         this.dao = dao;
     }
 
-    public boolean update(User employee) {
-        Validator<User> validator = new EmployeeInputRequestValidator();
-        Notification notification = validator.validate(employee);
+    public void update(User lastEmployee, User employee) {
+        Address employeeAddress = employee.getAddress();
 
-        if (notification.hasErros())
-            throw new IllegalArgumentException(notification.errorMessage());
+        if (!employee.getEmail().isBlank() && employee.getEmail() != null)
+            lastEmployee.setEmail(employee.getEmail());
+        if (!employee.getPhone().isBlank() && employee.getPhone() != null)
+            lastEmployee.setPhone(employee.getPhone());
+        if (!employeeAddress.getAddress().isBlank() && employeeAddress.getAddress() != null)
+            lastEmployee.getAddress().setAddress(employeeAddress.getAddress());
+        if (!employeeAddress.getNumber().isBlank() && employeeAddress.getNumber() != null)
+            lastEmployee.getAddress().setNumber(employeeAddress.getNumber());
+        if (!employeeAddress.getDistrict().isBlank() && employeeAddress.getDistrict() != null)
+            lastEmployee.getAddress().setDistrict(employeeAddress.getDistrict());
+        if (!employeeAddress.getCity().isBlank() && employeeAddress.getCity() != null)
+            lastEmployee.getAddress().setCity(employeeAddress.getCity());
+        lastEmployee.setActive(employee.isActive());
 
-        String login = employee.getLogin();
-        if (dao.findOneByLogin(login).isEmpty())
-            throw new EntityNotFoundException(login);
-
-        return dao.update(employee);
+        dao.update(lastEmployee);
     }
 }
