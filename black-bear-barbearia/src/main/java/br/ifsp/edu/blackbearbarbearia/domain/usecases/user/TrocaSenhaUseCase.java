1edcbb565d532b869e60ee1e5fb0c9904232f800
NathanHermes <hermesnathan8@gmail.com>
2022-11-08 21:59:41 -0300

    feat: implementação do método modifyPassword

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/user/TrocaSenhaUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/user/TrocaSenhaUseCase.java
@@ -3,19 +3,24 @@ package br.ifsp.edu.blackbearbarbearia.domain.usecases.user;
 import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
 
 public class TrocaSenhaUseCase {
-    private UserDAO dao;
+    private final UserDAO dao;
     public TrocaSenhaUseCase(UserDAO dao) {
         this.dao = dao;
     }
 
-    public boolean trocarSenha(User user, String password, String confirmPassword) {
-        if (password == null || password.isEmpty())
-            throw new IllegalArgumentException("Invalid password.");
-        if (confirmPassword == null || confirmPassword.isEmpty())
-            throw new IllegalArgumentException("Invalid confirm password.");
+    public Boolean modifyPassword(User user, String lastPassword, String newPassword, String confirmPassword) {
+        if (lastPassword == null || lastPassword.isBlank()) throw new IllegalArgumentException("Last password is null or empty");
+        if (newPassword == null || newPassword.isBlank()) throw new IllegalArgumentException("New password is null or empty");
+        if (confirmPassword == null || confirmPassword.isBlank()) throw new IllegalArgumentException("Confirm password is null or empty");
 
-        if (!password.equals(confirmPassword))
-            return false;
-        return true;
+        if (!newPassword.equals(confirmPassword)) throw new IllegalArgumentException("New password is not the same as confirm new password");
+        if (user.isValidPassword(lastPassword)) throw new IllegalArgumentException("Invalid last password");
+
+        user.setPasswordHash(newPassword);
+        user.setLastPassword(newPassword);
+
+        dao.update(user);
+
+        return Boolean.TRUE;
     }
 }
