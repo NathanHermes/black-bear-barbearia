84595e94bc064e99a5348a1331136b77a05a030f
NathanHermes <hermesnathan8@gmail.com>
2022-11-09 15:31:56 -0300

    feat: sobrecarga do método validate

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/utils/Validator.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/utils/Validator.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 public abstract class Validator <T>{
     public abstract Notification validate(T type);
+    public Notification validate(String lastPassword, String newPassword, String confirmNewPassword) {return null;};
+
     public static boolean nullOrEmpty(String string){
         return string == null || string.isEmpty();
     }
