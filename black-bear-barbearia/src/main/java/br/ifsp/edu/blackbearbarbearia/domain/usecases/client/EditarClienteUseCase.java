47632c6384c043bc54d59de296def005254b6a1e
PedroPiassi <pedro.a.piassi@gmail.com>
2022-11-09 17:57:52 -0300

    refactor: atribuindo valores, forma nova do id (como paramentro)

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/client/EditarClienteUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/client/EditarClienteUseCase.java
@@ -12,16 +12,21 @@ public class EditarClienteUseCase {
         this.dao = dao;
     }
 
-    public boolean update(Client client) {
+    public boolean update(Integer id, Client clientUpdate) {
         Validator<Client> validator = new ClientInputRequestValidator();
-        Notification notification = validator.validate(client);
+        Notification notification = validator.validate(clientUpdate);
 
         if (notification.hasErros())
             throw new IllegalArgumentException(notification.errorMessage());
 
-        Integer id = client.getId();
         if (dao.findOne(id).isEmpty())
             throw new EntityNotFoundException("Client not found.");
+
+        Client client = dao.findOne(id).get();
+
+        client.setName(clientUpdate.getName());
+        client.setPhone(clientUpdate.getPhone());
+        client.setEmail(clientUpdate.getEmail());
 
         return dao.update(client);
     }
