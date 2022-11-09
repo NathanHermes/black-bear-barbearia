de4e780a0cbbdfa3ab295b5de693f978cc1bfcaa
NathanHermes <hermesnathan8@gmail.com>
2022-11-09 15:33:01 -0300

    feat: dois métodos de validação para update do funcionário

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/UpdateEmployeeInputResquestValidator.java
@@ -0,0 +1,40 @@
+package br.ifsp.edu.blackbearbarbearia.domain.usecases.employee;
+
+import br.ifsp.edu.blackbearbarbearia.domain.entities.user.Address;
+import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
+public class UpdateEmployeeInputResquestValidator extends Validator<User> {
+    @Override
+    public Notification validate(User employee) {
+        Notification notification = new Notification();
+
+        if (employee == null) {
+            notification.addError("Employee is null");
+            return notification;
+        }
+
+        if (nullOrEmpty(employee.getEmail())) notification.addError("Email is null or empty");
+        if (nullOrEmpty(employee.getPhone())) notification.addError("Phone is null or empty");
+        if (employee.isActive() == null) notification.addError("Active is null");
+
+        Address employeeAddress = employee.getAddress();
+        if (nullOrEmpty(employeeAddress.getAddress())) notification.addError("Address is null or empty");
+        if (nullOrEmpty(employeeAddress.getNumber())) notification.addError("Number is null or empty");
+        if (nullOrEmpty(employeeAddress.getDistrict())) notification.addError("District is null or empty");
+        if (nullOrEmpty(employeeAddress.getCity())) notification.addError("City is null or empty");
+
+        return notification;
+    }
+
+    public Notification validate(String lastPassword, String newPassword, String confirmNewPassword) {
+        Notification notification = new Notification();
+
+        if (nullOrEmpty(lastPassword)) notification.addError("Last password is null or empty");
+        if (nullOrEmpty(newPassword)) notification.addError("New password is null or empty");
+        if (nullOrEmpty(confirmNewPassword)) notification.addError("Confirm new password is null or empty");
+
+        return notification;
+    }
+}
