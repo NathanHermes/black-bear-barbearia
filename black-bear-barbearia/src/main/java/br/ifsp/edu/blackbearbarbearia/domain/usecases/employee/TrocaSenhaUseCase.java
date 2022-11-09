da35c2e719e4a9dd265074560de6a4b02703de91
NathanHermes <hermesnathan8@gmail.com>
2022-11-09 15:33:30 -0300

    feat: nova abordagem para o tratamento das senhas

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/TrocaSenhaUseCase.java
@@ -0,0 +1,30 @@
+package br.ifsp.edu.blackbearbarbearia.domain.usecases.employee;
+
+import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.user.UserDAO;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
+public class TrocaSenhaUseCase {
+    private final UserDAO dao;
+    public TrocaSenhaUseCase(UserDAO dao) {
+        this.dao = dao;
+    }
+
+    public void modifyPassword(User user, String lastPassword, String newPassword, String confirmPassword) {
+        if (user.getPasswordHash() != null) throw new IllegalArgumentException("To change the password it needs to be deleted by the administrator");
+
+        Validator<User> validator = new UpdateEmployeeInputResquestValidator();
+        Notification notification = validator.validate(lastPassword, newPassword, confirmPassword);
+
+        if (notification.hasErros()) throw new IllegalArgumentException(notification.errorMessage());
+
+        if (!newPassword.equals(confirmPassword)) throw new IllegalArgumentException("New password is not the same as confirm new password");
+        if (user.isValidPassword(lastPassword)) throw new IllegalArgumentException("Invalid last password");
+
+        user.setPasswordHash(newPassword);
+        user.setLastPassword(newPassword);
+
+        dao.update(user);
+    }
+}
