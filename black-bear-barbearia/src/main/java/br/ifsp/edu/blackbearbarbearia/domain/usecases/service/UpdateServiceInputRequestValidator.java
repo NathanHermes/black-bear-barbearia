e9cac2d523aff003b6951cef2d6af105e45d6e5a
NathanHermes <hermesnathan8@gmail.com>
2022-11-09 18:48:15 -0300

    feat: classe de tratamento de entrada

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/UpdateServiceInputRequestValidator.java
@@ -0,0 +1,24 @@
+package br.ifsp.edu.blackbearbarbearia.domain.usecases.service;
+
+import br.ifsp.edu.blackbearbarbearia.domain.entities.service.Service;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
+public class UpdateServiceInputRequestValidator extends Validator<Service> {
+    @Override
+    public Notification validate(Service service) {
+        Notification notification = new Notification();
+
+        if (service == null) {
+            notification.addError("Service is null");
+            return notification;
+        }
+
+        if (nullOrEmpty(String.valueOf(service.getComissionPercentage()))) notification.addError("Commission is null or empty");
+        if (nullOrEmpty(String.valueOf(service.getTaxPercentage()))) notification.addError("Taxa percentage is null or empty");
+        if (nullOrEmpty(String.valueOf(service.getActive()))) notification.addError("Service status is null or empty");
+        if (service.getTypes().isEmpty()) notification.addError("The type of this service is empty");
+
+        return notification;
+    }
+}
