537d8daf077abad62adb43db2e5ab4c2da73f2db
NathanHermes <hermesnathan8@gmail.com>
2022-11-09 18:54:18 -0300

    feat: função findOne

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/ListarServicosUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/ListarServicosUseCase.java
@@ -3,6 +3,7 @@ package br.ifsp.edu.blackbearbarbearia.domain.usecases.service;
 import br.ifsp.edu.blackbearbarbearia.domain.entities.service.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ListarServicosUseCase {
     private final ServiceDAO dao;
@@ -15,8 +16,14 @@ public class ListarServicosUseCase {
         List<Service> services = dao.findAll();
 
         if (services.isEmpty())
-            throw new IllegalArgumentException("No service found.");
+            throw new IllegalArgumentException("Services not found");
 
         return services;
+    }
+
+    public Optional<Service> findOne(Integer serviceId) {
+        if (dao.findOne(serviceId).isEmpty()) throw new IllegalArgumentException("Service not found");
+
+        return dao.findOne(serviceId);
     }
 }
