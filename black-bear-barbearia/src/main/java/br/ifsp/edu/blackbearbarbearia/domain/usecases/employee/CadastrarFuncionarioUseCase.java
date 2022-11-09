c21839a052da3adb9c88d36cfd48325312453384
NathanHermes <hermesnathan8@gmail.com>
2022-11-08 21:51:05 -0300

    perf: pequena refatoração

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/CadastrarFuncionarioUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/CadastrarFuncionarioUseCase.java
@@ -19,21 +19,18 @@ public class CadastrarFuncionarioUseCase {
         Validator<User> validator = new EmployeeInputRequestValidator();
         Notification notification = validator.validate(employee);
 
-        if (notification.hasErros())
-            throw new IllegalArgumentException(notification.errorMessage());
+        if (notification.hasErros()) throw new IllegalArgumentException(notification.errorMessage());
 
         String email = employee.getEmail();
-        if (dao.findOneByEmail(email).isPresent())
-            throw new EntityAlreadyExistsException("This email is already in use.");
+        if (dao.findOneByEmail(email).isPresent()) throw new EntityAlreadyExistsException("This email is already in use");
 
         String login = employee.getLogin();
-        if (dao.findOneByLogin(login).isPresent())
-            throw new EntityAlreadyExistsException("This login is already in use.");
+        if (dao.findOneByLogin(login).isPresent()) throw new EntityAlreadyExistsException("This login is already in use");
 
         List<Day> daysEmployee = employee.getDays();
-        if (daysEmployee.stream().filter(day -> dao.findOneByDay(day).size() >= 3).count() == 1)
-            throw new IllegalArgumentException("Days with work limit reached.");
+        if (daysEmployee.stream().filter(day -> dao.findOneByDay(day).size() == 3).count() == 1) throw new IllegalArgumentException("Days with work limit reached");
 
+        employee.setLastPassword(employee.getPasswordHash());
         return dao.create(employee);
     }
 }
