38b3e737fa885419ed4739822f1d0afb121defdb
NathanHermes <hermesnathan8@gmail.com>
2022-11-08 21:56:56 -0300

    feat: implementação do método findOne

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/ListarFuncionariosUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/ListarFuncionariosUseCase.java
@@ -4,6 +4,7 @@ import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.user.UserDAO;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ListarFuncionariosUseCase {
     private final UserDAO dao;
@@ -12,6 +13,9 @@ public class ListarFuncionariosUseCase {
         this.dao = dao;
     }
 
+    public Optional<User> findOne(Integer id) {
+        return dao.findOne(id);
+    }
     public List<User> findAll() {
         List<User> users = dao.findAll();
 
