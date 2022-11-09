0792b07cedfaf1c6dd9ec575ddbbcdd7f10a9a5d
NathanHermes <hermesnathan8@gmail.com>
2022-11-08 21:54:36 -0300

    fix: modificação no tratamento das entradas das informações de endereço

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/EmployeeInputRequestValidator.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/employee/EmployeeInputRequestValidator.java
@@ -1,8 +1,11 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.employee;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.user.Address;
 import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
+import java.util.regex.Pattern;
 
 public class EmployeeInputRequestValidator extends Validator<User> {
     @Override
@@ -14,15 +17,19 @@ public class EmployeeInputRequestValidator extends Validator<User> {
             return notification;
         }
 
-        if (nullOrEmpty(employee.getFullName())) notification.addError("Full name is null or empty.");
-        if (nullOrEmpty(employee.getEmail())) notification.addError("Email is null or empty.");
-        if (nullOrEmpty(employee.getPhone())) notification.addError("Phone is null or empty.");
-        if (nullOrEmpty(employee.getAddress())) notification.addError("Address is null or empty.");
-        if (nullOrEmpty(employee.getNumber())) notification.addError("Number is null or empty.");
-        if (nullOrEmpty(employee.getDistrict())) notification.addError("District is null or empty.");
-        if (nullOrEmpty(employee.getCity())) notification.addError("City is null or empty.");
-        if (nullOrEmpty(employee.getLogin())) notification.addError("Login is null or empty.");
-        if (nullOrEmpty(employee.getPasswordHash())) notification.addError("Password is null or empty.");
+        Address employeeAddress = employee.getAddress();
+
+        if (nullOrEmpty(employee.getFullName())) notification.addError("Full name is null or empty");
+        if (nullOrEmpty(employee.getEmail())) notification.addError("Email is null or empty");
+        /*if (!Pattern.compile("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$").matcher(employee.getEmail()).matches())
+            notification.addError("Invalid email");*/
+        if (nullOrEmpty(employee.getPhone())) notification.addError("Phone is null or empty");
+        if (nullOrEmpty(employeeAddress.getAddress())) notification.addError("Address is null or empty");
+        if (nullOrEmpty(employeeAddress.getNumber())) notification.addError("Number is null or empty");
+        if (nullOrEmpty(employeeAddress.getDistrict())) notification.addError("District is null or empty");
+        if (nullOrEmpty(employeeAddress.getCity())) notification.addError("City is null or empty");
+        if (nullOrEmpty(employee.getLogin())) notification.addError("Login is null or empty");
+        if (nullOrEmpty(employee.getPasswordHash())) notification.addError("Password is null or empty");
 
         return notification;
     }
