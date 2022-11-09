7462b730636c2368fcee17ba2e61139c558ce5ef
dudasantos2522 <mariaeduardasantos0205@gmail.com>
2022-11-09 07:11:49 -0300

    Implementação de CriarAgendamentoUseCase

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/booking/CriarAgendamentoUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/booking/CriarAgendamentoUseCase.java
@@ -1,4 +1,32 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.booking;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.booking.Booking;
+import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.EntityAlreadyExistsException;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
+import java.time.LocalDate;
+
 public class CriarAgendamentoUseCase {
+    private final BookingDAO dao;
+    public CriarAgendamentoUseCase(BookingDAO dao) {
+        this.dao = dao;
+    }
+
+    public Integer create(Booking booking) {
+        Validator<Booking> validator = new BookingInputRequestValidator();
+        Notification notification = validator.validate(booking);
+
+        if (notification.hasErros()) throw new IllegalArgumentException(notification.errorMessage());
+
+        LocalDate date = booking.getDate();
+        User user = booking.getUser();
+        if (dao.findOneByDateAndUser(date, user).isPresent()) throw new EntityAlreadyExistsException("This date and user is already in use");
+
+        // Verificar dispinibilidade do user
+
+        return dao.create(booking);
+    }
 }
+
