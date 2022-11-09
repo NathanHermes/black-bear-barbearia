75f37b988bf71a10a0e1b6d42231e4254e50ef65
NathanHermes <hermesnathan8@gmail.com>
2022-11-09 15:34:34 -0300

    perf: variavel errors como final

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/utils/Notification.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/utils/Notification.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Notification {
-    private List<Error> errors = new ArrayList<>();
+    private final List<Error> errors = new ArrayList<>();
 
     public void addError(String message){
         addError(message, null);
