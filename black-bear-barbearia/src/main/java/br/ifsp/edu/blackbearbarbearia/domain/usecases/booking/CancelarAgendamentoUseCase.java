8f2426067758c6ee4b96156c3fe390fc5fcc2ae9
dudasantos2522 <mariaeduardasantos0205@gmail.com>
2022-11-09 07:12:29 -0300

    Implementação de CancelarAgendamentoUseCase

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/booking/CancelarAgendamentoUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/booking/CancelarAgendamentoUseCase.java
@@ -1,4 +1,27 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.booking;
 
+import br.ifsp.edu.blackbearbarbearia.domain.entities.booking.Booking;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.EntityNotFoundException;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
+import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
+
 public class CancelarAgendamentoUseCase {
+    private final BookingDAO dao;
+    public CancelarAgendamentoUseCase(BookingDAO dao) {
+        this.dao = dao;
+    }
+
+    public boolean delete(Booking booking) {
+        Validator<Booking> validator = new BookingInputRequestValidator();
+        Notification notification = validator.validate(booking);
+
+        if (notification.hasErros())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        Integer id = booking.getId();
+        if (dao.findOne(id).isEmpty())
+            throw new EntityNotFoundException("Booking not found.");
+
+        return dao.delete(booking);
+    }
 }
