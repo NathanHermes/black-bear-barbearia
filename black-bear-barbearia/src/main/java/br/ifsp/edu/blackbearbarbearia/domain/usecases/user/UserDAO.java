bc0d58466bea25c81d660f42f16b9a7ce6e82929
NathanHermes <hermesnathan8@gmail.com>
2022-11-24 11:48:49 -0300

    feat: criação e utilizção de um builder para a classe user

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/user/UserDAO.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/user/UserDAO.java
@@ -1,6 +1,5 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.user;
 
-import br.ifsp.edu.blackbearbarbearia.domain.entities.user.Day;
 import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.DAO;
 
