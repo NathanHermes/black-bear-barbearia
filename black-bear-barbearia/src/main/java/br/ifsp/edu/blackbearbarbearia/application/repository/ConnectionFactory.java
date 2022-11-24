0683804bfc8854350872f33bd0aced5e61bb0dab
dudasantos2522 <mariaeduardasantos0205@gmail.com>
2022-11-24 11:22:02 -0300

    DatabaseBuilder finalizado

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/repository/ConnectionFactory.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/repository/ConnectionFactory.java
@@ -1,4 +1,20 @@
 package br.ifsp.edu.blackbearbarbearia.application.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+    private static Connection connection;
+
+    public static Connection createConnection() throws SQLException {
+        if(connection == null)
+            connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+        return connection;
+    }
+
+    public static PreparedStatement createPreparedStatement(String sql) throws SQLException {
+        return createConnection().prepareStatement(sql);
+    }
 }
--- black-bear-barbearia/src/main/java/module-info.java
+++ black-bear-barbearia/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module br.ifsp.edu.blackbearbarbearia {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens br.ifsp.edu.blackbearbarbearia to javafx.fxml;
     exports br.ifsp.edu.blackbearbarbearia;
