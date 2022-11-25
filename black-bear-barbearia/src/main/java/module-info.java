73921699855d7e4ac31d2c8266ea709f8775a04c
NathanHermes <hermesnathan8@gmail.com>
2022-11-25 10:27:34 -0300

    style: altera linha da declaração

--- black-bear-barbearia/src/main/java/module-info.java
+++ black-bear-barbearia/src/main/java/module-info.java
@@ -5,9 +5,9 @@ module br.ifsp.edu.blackbearbarbearia {
 
     opens br.ifsp.edu.blackbearbarbearia.application.view to javafx.fxml;
     opens br.ifsp.edu.blackbearbarbearia.application.controller to javafx.fxml;
+    opens br.ifsp.edu.blackbearbarbearia.application.main to javafx.fxml;
 
     exports br.ifsp.edu.blackbearbarbearia.application.view;
     exports br.ifsp.edu.blackbearbarbearia.application.controller;
     exports br.ifsp.edu.blackbearbarbearia.application.main;
-    opens br.ifsp.edu.blackbearbarbearia.application.main to javafx.fxml;
 }
