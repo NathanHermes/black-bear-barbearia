8eaad5b77655c5450cc3c074e5b5ade590858ed6
PedroPiassi <pedro.a.piassi@gmail.com>
2022-12-01 12:40:24 -0300

    implementação da classe CadastrarFuncionarioController.java

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/CadastrarFuncionarioController.java
@@ -0,0 +1,72 @@
+package br.ifsp.edu.blackbearbarbearia.application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class CadastrarFuncionarioController {
+
+    @FXML
+    private TextField txtEndereco;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private Label lblStatus12;
+
+    @FXML
+    private Label lblStatus11;
+
+    @FXML
+    private Label lblCadastrar;
+
+    @FXML
+    private Button btnCadastrar;
+
+    @FXML
+    private TextField txtTel;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    private RadioButton rbAtivado;
+
+    @FXML
+    private TextField txtLogin;
+
+    @FXML
+    private TextField txtEndereco11;
+
+    @FXML
+    private TextField txtEndereco13;
+
+    @FXML
+    private TextField txtEndereco1;
+
+    @FXML
+    private TextField txtEndereco12;
+
+    @FXML
+    private RadioButton rbDesativado;
+
+    @FXML
+    private Label lblStatus1;
+
+    @FXML
+    void cadastrar(ActionEvent event) {
+
+    }
+}
+
