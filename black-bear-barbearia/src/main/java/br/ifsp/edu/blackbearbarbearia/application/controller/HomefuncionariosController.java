1c368303a66e9666b4320aaa935040de197e71d5
PedroPiassi <pedro.a.piassi@gmail.com>
2022-12-01 12:42:57 -0300

    implementação da classe HomefuncionariosController.java

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/HomefuncionariosController.java
@@ -0,0 +1,58 @@
+package br.ifsp.edu.blackbearbarbearia.application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class HomefuncionariosController {
+
+    @FXML
+    private Label lblPesquisar;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private TableColumn<?, ?> cNome;
+
+    @FXML
+    private Button btnCadastrar;
+
+    @FXML
+    private TextField txtPesquisar;
+
+    @FXML
+    private Button btnApagarSenha;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private Button btnEditar;
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void apagarSenha(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cadastrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editar(ActionEvent event) {
+
+    }
+
+}
+
