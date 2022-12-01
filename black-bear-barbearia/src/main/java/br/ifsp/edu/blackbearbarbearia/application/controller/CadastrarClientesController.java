50d6ff7b8806da6b69eb33f621c193453aa49de8
PedroPiassi <pedro.a.piassi@gmail.com>
2022-12-01 12:39:02 -0300

    implementação da classe CadastrarClientesController.java

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/CadastrarClientesController.java
@@ -0,0 +1,32 @@
+package br.ifsp.edu.blackbearbarbearia.application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CadastrarClientesController {
+
+    @FXML
+    private Label lblCadastrar;
+
+    @FXML
+    private Button btnCadastrar;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtTelefone;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    void cadastrar(ActionEvent event) {
+
+    }
+
+}
+
