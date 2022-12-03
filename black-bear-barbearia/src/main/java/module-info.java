7ddf4a4a9f794ccfb6503af748b77a9aedd4f450
NathanHermes <hermesnathan8@gmail.com>
2022-12-03 09:15:30 -0300

    feat: adiciona exportações das classes

--- black-bear-barbearia/src/main/java/module-info.java
+++ black-bear-barbearia/src/main/java/module-info.java
@@ -5,9 +5,23 @@ module br.ifsp.edu.blackbearbarbearia {
 
     opens br.ifsp.edu.blackbearbarbearia.application.view to javafx.fxml;
     opens br.ifsp.edu.blackbearbarbearia.application.controller to javafx.fxml;
+    opens br.ifsp.edu.blackbearbarbearia.application.view.css to javafx.fxml;
     opens br.ifsp.edu.blackbearbarbearia.application.main to javafx.fxml;
 
     exports br.ifsp.edu.blackbearbarbearia.application.view;
     exports br.ifsp.edu.blackbearbarbearia.application.controller;
     exports br.ifsp.edu.blackbearbarbearia.application.main;
+
+    exports br.ifsp.edu.blackbearbarbearia.domain.usecases.user;
+    exports br.ifsp.edu.blackbearbarbearia.domain.usecases.employee;
+    exports br.ifsp.edu.blackbearbarbearia.domain.usecases.client;
+    exports br.ifsp.edu.blackbearbarbearia.domain.usecases.service;
+    exports br.ifsp.edu.blackbearbarbearia.domain.usecases.booking;
+    exports br.ifsp.edu.blackbearbarbearia.domain.usecases.utils;
+
+    exports br.ifsp.edu.blackbearbarbearia.domain.entities.user;
+    exports br.ifsp.edu.blackbearbarbearia.domain.entities.client;
+    exports br.ifsp.edu.blackbearbarbearia.domain.entities.service;
+    exports br.ifsp.edu.blackbearbarbearia.domain.entities.booking;
+
 }
