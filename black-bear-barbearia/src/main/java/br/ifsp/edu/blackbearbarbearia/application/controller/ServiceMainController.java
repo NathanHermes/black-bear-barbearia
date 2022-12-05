0e4f1a7c7a8370df22d9e434fc15c4f3b26e925e
NathanHermes <hermesnathan8@gmail.com>
2022-12-05 16:52:41 -0300

    style: altera nome da classe

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/ServiceMainController.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/ServiceMainController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class HomeServicosController {
+public class ServiceMainController {
 
     @FXML
     private TableColumn<?, ?> cPreco;
