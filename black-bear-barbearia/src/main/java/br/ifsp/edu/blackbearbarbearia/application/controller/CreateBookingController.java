848748f0ea466224f1fb925115c05863e606bdc0
Pedro Piassi <41785409+PedroPiassi@users.noreply.github.com>
2022-12-05 15:30:09 -0300

    feat: controller de cadastrar reserva

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/CreateBookingController.java
@@ -0,0 +1,41 @@
+package br.ifsp.edu.blackbearbarbearia.application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class CreateBookingController {
+
+    @FXML
+    private Label lblCadastrar;
+
+    @FXML
+    private ComboBox<?> cbCliente;
+
+    @FXML
+    private ComboBox<?> cbServico;
+
+    @FXML
+    private Button btnCadastrar;
+
+    @FXML
+    private ComboBox<?> cbFuncionario;
+
+    @FXML
+    private ComboBox<?> cbHorario;
+
+    @FXML
+    private Label txtValor;
+
+    @FXML
+    private DatePicker dtData;
+
+    @FXML
+    void cadastrar(ActionEvent event) {
+
+    }
+}
+
