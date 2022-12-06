18eac8695e0006084d4cbe3d0e8509bd5a8cbf89
NathanHermes <hermesnathan8@gmail.com>
2022-12-06 20:03:48 -0300

    refactor: altera nome do método

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/user/UserDAO.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/user/UserDAO.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 public interface UserDAO extends DAO<User, Integer> {
     Optional<User> findByLogin(String login);
     Optional<User> findByEmail(String email);
-    Integer findByDay(Integer day);
+    Integer findCountByDay(Integer day);
 }
