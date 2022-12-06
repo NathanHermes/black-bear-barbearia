ebbe5b652385d156d928b8c7cf8888d364d42985
NathanHermes <hermesnathan8@gmail.com>
2022-12-06 18:43:54 -0300

    refactor: altera nome das funções

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/user/UserDAO.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/user/UserDAO.java
@@ -3,12 +3,10 @@ package br.ifsp.edu.blackbearbarbearia.domain.usecases.user;
 import br.ifsp.edu.blackbearbarbearia.domain.entities.user.User;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.DAO;
 
-import java.time.DayOfWeek;
-import java.util.List;
 import java.util.Optional;
 
 public interface UserDAO extends DAO<User, Integer> {
-    Optional<User> findOneByLogin(String login);
-    Optional<User> findOneByEmail(String email);
-    List<User> findOneByDay(DayOfWeek day);
+    Optional<User> findByLogin(String login);
+    Optional<User> findByEmail(String email);
+    Integer findByDay(Integer day);
 }
