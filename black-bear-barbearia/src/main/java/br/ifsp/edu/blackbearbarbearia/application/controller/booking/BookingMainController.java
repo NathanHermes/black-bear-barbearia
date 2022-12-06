8ea1454fac21385339aed61613f18203eae22001
NathanHermes <hermesnathan8@gmail.com>
2022-12-06 08:59:22 -0300

    refactor: altera diretório da classe

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/booking/BookingMainController.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/booking/BookingMainController.java
@@ -1,4 +1,4 @@
-package br.ifsp.edu.blackbearbarbearia.application.controller;
+package br.ifsp.edu.blackbearbarbearia.application.controller.booking;
 
 import br.ifsp.edu.blackbearbarbearia.application.view.WindowLoader;
 import br.ifsp.edu.blackbearbarbearia.domain.entities.booking.Booking;
