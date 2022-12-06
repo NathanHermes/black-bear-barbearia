505ca8f390c97a2aafd84a91fef393de320ffe32
NathanHermes <hermesnathan8@gmail.com>
2022-12-05 21:19:13 -0300

    refactor: altera tipo de retorno da função

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/utils/DAO.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/utils/DAO.java
@@ -1,10 +1,11 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.utils;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface DAO<T, K>{
-    K create(T type);
+    Boolean create(T type);
     Boolean update(T type);
     Boolean deleteByKey(K key);
     Boolean delete(T type);
