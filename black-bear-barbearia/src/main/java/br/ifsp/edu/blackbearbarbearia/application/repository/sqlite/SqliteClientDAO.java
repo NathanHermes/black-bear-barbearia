74f95943d7e22611096811a1b3433ab5847deab4
NathanHermes <hermesnathan8@gmail.com>
2022-12-05 21:11:41 -0300

    style: deleta uma linha em branco

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/repository/sqlite/SqliteClientDAO.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/repository/sqlite/SqliteClientDAO.java
@@ -132,7 +132,6 @@ public class SqliteClientDAO implements ClientDAO {
 
                 clients.add(client);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
