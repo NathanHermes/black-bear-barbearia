db256ddab404a9b8fea726fc8fd531e377605121
Pedro Piassi <41785409+PedroPiassi@users.noreply.github.com>
2022-12-06 08:57:53 -0300

    feat: controller da tela de error

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/ErrorMessageController.java
@@ -0,0 +1,25 @@
+package br.ifsp.edu.blackbearbarbearia.application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class ErrorMessageController {
+
+    @FXML
+    private ImageView imgError;
+
+    @FXML
+    private Text txtMensage;
+
+    @FXML
+    private Button btnOk;
+
+    @FXML
+    void ok(ActionEvent event) {
+
+    }
+
+}
