c901521d6f2aae03686c731b4a146543860a1bef
Pedro Piassi <41785409+PedroPiassi@users.noreply.github.com>
2022-12-07 16:02:14 -0300

    feat: inciando controller de report

---
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/application/controller/booking/ReportController.java
@@ -0,0 +1,46 @@
+package br.ifsp.edu.blackbearbarbearia.application.controller.booking;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class ReportController {
+
+    @FXML
+    private Text txtQtdd;
+
+    @FXML
+    private Text txtEmployee;
+
+    @FXML
+    private Text txtComission;
+
+    @FXML
+    private Text txtDate;
+
+    @FXML
+    private Text txtService;
+
+    @FXML
+    private Text yxyClient;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Text txtValue;
+
+    @FXML
+    private Text txtTime;
+
+    @FXML
+    void scroll(ActionEvent event) {
+
+    }
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+}
+
