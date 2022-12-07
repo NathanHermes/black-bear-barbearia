67ed904cc378bd04d51b93d3445c4217c63ce94f
Pedro Piassi <41785409+PedroPiassi@users.noreply.github.com>
2022-12-06 23:07:23 -0300

    refactor: acrescimento do retorno e servicBuilder

--- black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/UpdateServiceUseCase.java
+++ black-bear-barbearia/src/main/java/br/ifsp/edu/blackbearbarbearia/domain/usecases/service/UpdateServiceUseCase.java
@@ -1,18 +1,19 @@
 package br.ifsp.edu.blackbearbarbearia.domain.usecases.service;
 
 import br.ifsp.edu.blackbearbarbearia.domain.entities.service.Service;
+import br.ifsp.edu.blackbearbarbearia.domain.entities.service.ServiceBuilder;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.EntityNotFoundException;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Notification;
 import br.ifsp.edu.blackbearbarbearia.domain.usecases.utils.Validator;
 
-public class EditarServicoUseCase {
+public class UpdateServiceUseCase {
     private final ServiceDAO dao;
 
-    public EditarServicoUseCase(ServiceDAO dao) {
+    public UpdateServiceUseCase(ServiceDAO dao) {
         this.dao = dao;
     }
 
-    public void update(Integer serviceId, Service serviceUpdate) {
+    public Boolean update(Integer serviceId, Service serviceUpdate) {
         Validator<Service> validator = new UpdateServiceInputRequestValidator();
         Notification notification = validator.validate(serviceUpdate);
 
@@ -22,14 +23,15 @@ public class EditarServicoUseCase {
         if(dao.findOne(serviceId).isEmpty())
             throw new EntityNotFoundException("Service not found");
 
-        Service service = dao.findOne(serviceId).get();
+        ServiceBuilder serviceBuilder = new ServiceBuilder();
+        serviceBuilder.setId(serviceId);
+        serviceBuilder.setName(serviceUpdate.getName());
+        serviceBuilder.setPrice(serviceUpdate.getPrice());
+        serviceBuilder.setComissionPercentage(serviceUpdate.getComissionPercentage());
+        serviceBuilder.setTaxPercentage(serviceUpdate.getTaxPercentage());
+        serviceBuilder.setActive(serviceUpdate.getActive());
+        serviceBuilder.setTypes(serviceUpdate.getTypes());
 
-        service.setComissionPercentage(serviceUpdate.getComissionPercentage());
-        service.setTaxPercentage(serviceUpdate.getTaxPercentage());
-        service.setActive(serviceUpdate.getActive());
-        service.getTypes().clear();
-        serviceUpdate.getTypes().forEach(service::addType);
-
-        dao.update(service);
+        return dao.update(serviceBuilder.getResult());
     }
 }
